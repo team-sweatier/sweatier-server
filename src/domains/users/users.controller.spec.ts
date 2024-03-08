622334a9b63d819f6c2b0297543683853ddd0502
jimyungkoh <kojimyung1206@naver.com>
2024-03-08 17:21:13 +0900

    fix: 모듈 import path 수정

--- src/domains/users/users.controller.spec.ts
+++ src/domains/users/users.controller.spec.ts
@@ -1,5 +1,5 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { UsersController } from './UsersController';
+import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 
 describe('UsersController', () => {
