b4f6566f62a5bf6ef8034801cf4332b5e5e20f24
sangddong <hehe74029@gmail.com>
2024-03-08 20:39:58 +0900

    fix: 오타 수정

--- src/domains/matches/matches-error.messages.ts
+++ src/domains/matches/matches-error.messages.ts
@@ -1,6 +1,6 @@
 export const INVALID_TITLE_FORMAT = '제목을 입력해주세요.';
 
-export const INVALID_CONTENT_FORMAT = '내용을 입력해주세요';
+export const INVALID_CONTENT_FORMAT = '내용을 입력해주세요.';
 
 export const INVALID_APPLICATION = '신청할 수 없는 매치입니다.';
 
@@ -24,3 +24,19 @@ export const INVALID_RATING = '평가할 수 없는 유저입니다.';
 export const ALREADY_RATED = '이미 평가한 유저입니다.';
 
 export const PROFILE_NEEDED = '프로필을 생성해주세요.';
+
+export const INVALID_TITLE = '제목은 다섯글자 이상 입력해주세요.';
+
+export const INVALID_CONTENT = '내용은 열글자 이상 입력해주세요.';
+
+export const CHOOSE_GENDER = '성별을 선택해주세요.';
+
+export const INVALID_CAPABILITY = '모집 인원을 두 명 이상 입력해주세요.';
+
+export const CHOOSE_SPORTSTYPE = '종목을 설정해주세요.';
+
+export const CHOOSE_TIER = '티어를 설정해주세요.';
+
+export const INVALID_ADDRESS = '주소를 입력해주세요.';
+
+export const INVALID_RATE = '별점은 1점부터 5점까지 매길 수 있습니다.';
