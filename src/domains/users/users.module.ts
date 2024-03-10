02a40da84e8c1513f260111535fce989acebc2a2
mpfo0106 <mpfo0106@gmail.com>
2024-03-10 19:18:36 +0900

    feat: profile img 등록 gcs 사용하도록 설정

--- src/domains/users/users.module.ts
+++ src/domains/users/users.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { StorageModule } from 'src/storage/storage.module';
 import { KakaoAuthService } from './kakao-auth/kakao-auth.service';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
@@ -6,5 +7,6 @@ import { UsersService } from './users.service';
 @Module({
   controllers: [UsersController],
   providers: [UsersService, KakaoAuthService],
+  imports: [StorageModule],
 })
 export class UsersModule {}
