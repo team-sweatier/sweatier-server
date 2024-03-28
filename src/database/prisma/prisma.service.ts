c6837acf3bab6674e59bbf6679768e5184ed7a13
mpfo0106 <mpfo0106@gmail.com>
2024-03-28 22:47:40 +0900

    feat: prisma middleware에 UTCDateToKST 로직 추가

--- src/database/prisma/prisma.service.ts
+++ src/database/prisma/prisma.service.ts
@@ -1,5 +1,6 @@
 import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
+import { adjustDates, convertUTCDateToKST } from './date.middleware';
 
 @Injectable()
 export class PrismaService
@@ -8,9 +9,19 @@ export class PrismaService
 {
   async onModuleInit() {
     await this.$connect();
+    this.setMiddlewares();
   }
 
   async onModuleDestroy() {
     await this.$disconnect();
+  }
+  setMiddlewares() {
+    this.$use(async (params, next) => {
+      const result = await next(params);
+
+      adjustDates(result, convertUTCDateToKST);
+
+      return result;
+    });
   }
 }
