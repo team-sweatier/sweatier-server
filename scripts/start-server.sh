79865b303761b4e7e054ef6c029b1e91f36a95cb
mpfo0106 <mpfo0106@gmail.com>
2024-09-29 02:14:32 +0900

    feat: script 에 환경변수 불러오기

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 echo "--------------- 서버 배포 시작 -----------------"
+# .env 파일이 존재하는지 확인하고 환경 변수를 불러옵니다
+if [ -f "/home/ubuntu/sweatier-server/.env" ]; then
+  export $(xargs < /home/ubuntu/sweatier-server/.env)
+fi
 docker stop sweatier-server || true
 docker rm sweatier-server || true
 docker pull ${ECR_REGISTRY}/sweatier-server:latest
