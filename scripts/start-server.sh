c51e4258ea9d1a29a6d64775fe22dd7449a8f50c
mpfo0106 <mpfo0106@gmail.com>
2024-09-29 02:36:43 +0900

    fix: 기존 env 환경변수 방식에서 txt 방식으로 변경

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
+set -e
 
 echo "--------------- 서버 배포 시작 -----------------"
-# .env 파일이 존재하는지 확인하고 환경 변수를 불러옵니다
-if [ -f "/home/ubuntu/sweatier-server/.env" ]; then
-  export $(xargs < /home/ubuntu/sweatier-server/.env)
-fi
+IMAGE_FILE_PATH="/home/ubuntu/sweatier-server/image.txt"
+IMAGE_NAME=$(cat "$IMAGE_FILE_PATH")
+
 docker stop sweatier-server || true
 docker rm sweatier-server || true
-docker pull ${ECR_REGISTRY}/sweatier-server:latest
-docker run -d --name sweatier-server -p 3000:3000 ${ECR_REGISTRY}/sweatier-server:latest
+
+docker pull $IMAGE_NAME
+docker run -d --name sweatier-server -p 3000:3000 $IMAGE_NAME
